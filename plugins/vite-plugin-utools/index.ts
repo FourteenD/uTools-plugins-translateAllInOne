d07e77d9ee5bed0283864c9073774d4adcd61768
FourteenD <2899385645@qq.com>
2022-08-29 18:00:26 +0800

    up

--- plugins/vite-plugin-utools/index.ts
+++ plugins/vite-plugin-utools/index.ts
@@ -1,14 +1,6 @@
 import { Plugin } from "vite";
-interface Options {
-  watch?: boolean;
-  preloadPath?: string;
-  pluginPath?: string;
-}
-export default function utools(options: Options): Plugin {
-  // 默认配置
-  options.watch = options.watch || true;
-  options.preloadPath = options.preloadPath || "./src/preload.ts";
-  options.pluginPath = options.pluginPath || "./plugin.config.ts";
+
+export default function utools(options = {}): Plugin {
   const virtualModuleId = "virtual:my-module";
   const resolvedVirtualModuleId = "\0" + virtualModuleId;
 
--- plugins/vite-plugin-utools/utils.ts
+++ plugins/vite-plugin-utools/utils.ts
@@ -1,8 +1,31 @@
-export type Data = Record<any, any>;
-
+interface Options {
+  watch?: boolean;
+  preloadPath?: string;
+  pluginPath?: string;
+}
 export const isUndef = (val: unknown): val is undefined | null => val == void 0;
 
-export const isObject = (val: unknown): val is Data =>
+export const isObject = (val: unknown): val is Record<any, any> =>
   !!val && typeof val === "object";
 export const isString = (val: unknown): val is string =>
   typeof val === "string";
+
+const defaultOptions: Options = {
+  watch: true,
+  preloadPath: "./src/preload.ts",
+  pluginPath: "/plugin.config.ts",
+};
+
+export const resolveOptions = (options: Options): RequiredOptions =>
+  Object.entries(defaultOptions).reduce((ret, [key, v1]) => {
+    // @ts-ignore
+    const v2 = options[key];
+
+    ret[key] = isUndef(v2)
+      ? v1
+      : isObject(v1) && isObject(v2)
+      ? { ...v1, ...v2 }
+      : v2;
+
+    return ret;
+  }, {} as any);
