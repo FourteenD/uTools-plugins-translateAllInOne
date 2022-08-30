1493dde37b97eca66b1e2c708710b28c282340f3
FourteenD <2899385645@qq.com>
2022-08-30 14:59:06 +0800

    up

--- plugins/vite-plugin-utools/transform/external.ts
+++ plugins/vite-plugin-utools/transform/external.ts
@@ -1,66 +1,77 @@
-import { NodePath, PluginObj, transformAsync, types as t } from '@babel/core';
-import { genStatements } from '../helper';
+import { NodePath, PluginObj, transformAsync, types as t } from "@babel/core";
+import { genStatements } from "../helper";
 
 type SourceExternal = (source: string) => string | void | undefined | null;
-type Specifier = t.ImportDefaultSpecifier | t.ImportSpecifier | t.ImportNamespaceSpecifier | t.ExportSpecifier;
+type Specifier =
+  | t.ImportDefaultSpecifier
+  | t.ImportSpecifier
+  | t.ImportNamespaceSpecifier
+  | t.ExportSpecifier;
 
 const genExternalTemp = (external: string, specifiers: Specifier[]) =>
-	specifiers.reduce((temp, specifier) => {
-		const localName = specifier.local.name;
-		let varName = localName,
-			externalName = external;
+  specifiers.reduce((temp, specifier) => {
+    const localName = specifier.local.name;
+    let varName = localName,
+      externalName = external;
 
-		if (t.isExportSpecifier(specifier)) {
-			temp += 'export ';
-			//@ts-ignore
-			varName = specifier.exported.name;
+    if (t.isExportSpecifier(specifier)) {
+      temp += "export ";
+      //@ts-ignore
+      varName = specifier.exported.name;
 
-			if (localName === 'default') {
-				externalName += `["${localName}"]`;
-			}
-		} else {
-			//@ts-ignore
-			const importedName = t.isImportDefaultSpecifier(specifier) ? 'default' : specifier.imported.name;
+      if (localName === "default") {
+        externalName += `["${localName}"]`;
+      }
+    } else {
+      //@ts-ignore
+      const importedName = t.isImportDefaultSpecifier(specifier)
+        ? "default"
+        : specifier.imported.name;
 
-			externalName += `["${importedName}"]`;
-		}
+      externalName += `["${importedName}"]`;
+    }
 
-		temp += `const ${varName} = ${externalName};`;
+    temp += `const ${varName} = ${externalName};`;
 
-		return temp;
-	}, '');
+    return temp;
+  }, "");
 
-export const transformImportToExternal = (sourceExternal: SourceExternal): PluginObj => {
-	const transform = (path: NodePath<any>) => {
-		const { node } = path;
-		const external = node.source && sourceExternal(node.source.value);
+export const transformImportToExternal = (
+  sourceExternal: SourceExternal
+): PluginObj => {
+  const transform = (path: NodePath<any>) => {
+    const { node } = path;
+    const external = node.source && sourceExternal(node.source.value);
 
-		if (!external) return;
+    if (!external) return;
 
-		const { specifiers } = node;
+    const { specifiers } = node;
 
-		const temp = genExternalTemp(external, specifiers as any);
+    const temp = genExternalTemp(external, specifiers as any);
 
-		temp && path.replaceWithMultiple(genStatements(temp));
-	};
+    temp && path.replaceWithMultiple(genStatements(temp));
+  };
 
-	return {
-		name: 'transform-import-to-external',
+  return {
+    name: "transform-import-to-external",
 
-		visitor: {
-			ImportDeclaration: transform,
+    visitor: {
+      ImportDeclaration: transform,
 
-			ExportNamedDeclaration: transform,
-		},
-	};
+      ExportNamedDeclaration: transform,
+    },
+  };
 };
 
-export const transformExternal = async (sourceCode: string, sourceExternal: SourceExternal) => {
-	const result = await transformAsync(sourceCode, {
-		plugins: [transformImportToExternal(sourceExternal)],
-	});
+export const transformExternal = async (
+  sourceCode: string,
+  sourceExternal: SourceExternal
+) => {
+  const result = await transformAsync(sourceCode, {
+    plugins: [transformImportToExternal(sourceExternal)],
+  });
 
-	return result?.code;
+  return result?.code;
 };
 
 export default transformExternal;
