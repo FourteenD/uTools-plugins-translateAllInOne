4b8f684561c4c9d06ab10f405a88cfa09cf2f647
FourteenD <2899385645@qq.com>
2022-08-30 14:55:31 +0800

    vite-plugin-utools init

--- plugins/vite-plugin-utools/buildUpx.ts
+++ plugins/vite-plugin-utools/buildUpx.ts
@@ -1,99 +1,128 @@
-import { createPackage } from 'asar';
-import { resolve as pathResolve, basename, isAbsolute } from 'path';
-import { createReadStream, createWriteStream } from 'fs';
-import { unlink, mkdir, writeFile } from 'fs/promises';
-import { createGzip } from 'zlib';
-import { BuildUpxOptions, NestedRequired, PluginOptions, RequiredOptions } from './options';
-import { ResolvedConfig } from 'vite';
-import { Data, isString } from './helper';
-import colors from 'picocolors';
+import { createPackage } from "asar";
+import { resolve as pathResolve, basename, isAbsolute } from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { unlink, mkdir, writeFile } from "fs/promises";
+import { createGzip } from "zlib";
+import {
+  BuildUpxOptions,
+  NestedRequired,
+  PluginOptions,
+  RequiredOptions,
+} from "./options";
+import { ResolvedConfig } from "vite";
+import { Data, isString } from "./helper";
+import colors from "picocolors";
 
 const cwd = process.cwd();
 
 const requiredKeys = [
-	'name',
-	'pluginName',
-	'description',
-	'author',
-	'homepage',
-	'version',
-	'logo',
-	'features',
+  "name",
+  "pluginName",
+  "description",
+  "author",
+  "homepage",
+  "version",
+  "logo",
+  "features",
 ] as const;
-const DOC_URL = 'https://www.u.tools/docs/developer/config.html#基本配置';
+const DOC_URL = "https://www.u.tools/docs/developer/config.html#基本配置";
 const validatePluginOptions = (options: PluginOptions) => {
-	requiredKeys.forEach((key) => {
-		if (!options[key]) throw new Error(colors.red(`plugin ${key} is required,see: ${colors.bold(DOC_URL)}`));
-	});
+  requiredKeys.forEach((key) => {
+    if (!options[key])
+      throw new Error(
+        colors.red(`plugin ${key} is required,see: ${colors.bold(DOC_URL)}`)
+      );
+  });
 };
 
 const formatPluginOptions = (pluginOptions: Data, needPreload: boolean) => {
-	pluginOptions.main = 'index.html';
-	pluginOptions.logo = basename(pluginOptions.logo);
-	pluginOptions.preload = needPreload ? 'preload.js' : void 0;
+  pluginOptions.main = "index.html";
+  pluginOptions.logo = basename(pluginOptions.logo);
+  pluginOptions.preload = needPreload ? "preload.js" : void 0;
 
-	return pluginOptions as PluginOptions;
+  return pluginOptions as PluginOptions;
 };
 
 const getPluginOptions = (path: string) => {
-	const requirePath = isAbsolute(path) ? path : pathResolve(cwd, path);
-	const pluginOptions = require(requirePath);
-	validatePluginOptions(pluginOptions);
+  const requirePath = isAbsolute(path) ? path : pathResolve(cwd, path);
+  const pluginOptions = require(requirePath);
+  validatePluginOptions(pluginOptions);
 
-	return pluginOptions;
+  return pluginOptions;
 };
 
 const writePluginJson = (pluginOptions: PluginOptions, to: string) =>
-	writeFile(`${to}/plugin.json`, JSON.stringify(pluginOptions), 'utf8');
+  writeFile(`${to}/plugin.json`, JSON.stringify(pluginOptions), "utf8");
 
 const tempRE = /\[(\w+)\]/g;
 const generateOutName = (temp: string, pluginOptions: PluginOptions) =>
-	temp.replace(tempRE, (str, key: keyof PluginOptions) => {
-		const value = pluginOptions[key];
+  temp.replace(tempRE, (str, key: keyof PluginOptions) => {
+    const value = pluginOptions[key];
 
-		return isString(value) ? value : str;
-	});
+    return isString(value) ? value : str;
+  });
 
-const prepareOutDir = async (buildOptions: NestedRequired<BuildUpxOptions>, pluginOptions: PluginOptions) => {
-	await mkdir(buildOptions.outDir, { recursive: true });
+const prepareOutDir = async (
+  buildOptions: NestedRequired<BuildUpxOptions>,
+  pluginOptions: PluginOptions
+) => {
+  await mkdir(buildOptions.outDir, { recursive: true });
 
-	return pathResolve(buildOptions.outDir, generateOutName(buildOptions.outName, pluginOptions));
+  return pathResolve(
+    buildOptions.outDir,
+    generateOutName(buildOptions.outName, pluginOptions)
+  );
 };
 
 const TEMPORARY_DEST = pathResolve(cwd, `./.utools_${Math.random()}`);
 
 const doBuild = async (input: string, out: string) => {
-	await createPackage(input, TEMPORARY_DEST);
+  await createPackage(input, TEMPORARY_DEST);
 
-	await new Promise((resolve, reject) =>
-		createReadStream(TEMPORARY_DEST)
-			.pipe(createGzip())
-			.pipe(createWriteStream(out))
-			.on('error', reject)
-			.on('finish', resolve)
-	).finally(() => unlink(TEMPORARY_DEST));
+  await new Promise((resolve, reject) =>
+    createReadStream(TEMPORARY_DEST)
+      .pipe(createGzip())
+      .pipe(createWriteStream(out))
+      .on("error", reject)
+      .on("finish", resolve)
+  ).finally(() => unlink(TEMPORARY_DEST));
 };
 
-export const buildUpx = async (input: string, options: RequiredOptions, logger: ResolvedConfig['logger']) => {
-	const { buildUpx: buildOptions, preload } = options;
-	if (!buildOptions) return;
+export const buildUpx = async (
+  input: string,
+  options: RequiredOptions,
+  logger: ResolvedConfig["logger"]
+) => {
+  const { buildUpx: buildOptions, preload } = options;
+  if (!buildOptions) return;
 
-	logger.info(colors.green('\nbuilding for upx....'));
+  logger.info(colors.green("\nbuilding for upx...."));
 
-	try {
-		const pluginOptions = formatPluginOptions(getPluginOptions(buildOptions.pluginPath), !!preload);
-		logger.info(`${colors.green('plugin.json for building upx:')}\n${JSON.stringify(pluginOptions, null, 2)}`);
+  try {
+    const pluginOptions = formatPluginOptions(
+      getPluginOptions(buildOptions.pluginPath),
+      !!preload
+    );
+    logger.info(
+      `${colors.green("plugin.json for building upx:")}\n${JSON.stringify(
+        pluginOptions,
+        null,
+        2
+      )}`
+    );
 
-		await writePluginJson(pluginOptions, input);
+    await writePluginJson(pluginOptions, input);
 
-		const out = await prepareOutDir(buildOptions, pluginOptions);
-		await doBuild(input, out);
+    const out = await prepareOutDir(buildOptions, pluginOptions);
+    await doBuild(input, out);
 
-		logger.info(`${colors.green('✓')} build upx success`);
-		logger.info(colors.magenta(out));
-	} catch (error: any) {
-		logger.error(`${colors.red('build upx failed:')}\n${error.stack || error.message}`);
-	}
+    logger.info(`${colors.green("✓")} build upx success`);
+    logger.info(colors.magenta(out));
+  } catch (error: any) {
+    logger.error(
+      `${colors.red("build upx failed:")}\n${error.stack || error.message}`
+    );
+  }
 };
 
 export default buildUpx;
--- plugins/vite-plugin-utools/options.ts
+++ plugins/vite-plugin-utools/options.ts
@@ -1,60 +1,64 @@
-import { Data, isObject, isUndef } from './helper';
+import { Data, isObject, isUndef } from "./helper";
 
 export interface PreloadOptions {
-	path?: string;
-	watch?: boolean;
-	name?: string;
+  path?: string;
+  watch?: boolean;
+  name?: string;
 }
 
 export interface PluginOptions {
-	name: string;
-	logo: string;
-	features: Data;
-	pluginName?: string;
-	description?: string;
-	author?: string;
-	homepage?: string;
-	version?: string;
+  name: string;
+  logo: string;
+  features: Data;
+  pluginName?: string;
+  description?: string;
+  author?: string;
+  homepage?: string;
+  version?: string;
 }
 
 export interface BuildUpxOptions {
-	pluginPath?: string;
-	outDir?: string;
-	outName?: string;
+  pluginPath?: string;
+  outDir?: string;
+  outName?: string;
 }
 
 export interface Options {
-	external?: string;
-	preload?: PreloadOptions | false;
-	buildUpx?: BuildUpxOptions | false;
+  external?: string;
+  preload?: PreloadOptions | false;
+  buildUpx?: BuildUpxOptions | false;
 }
 
 export type NestedRequired<T> = {
-	[P in keyof T]-?: NestedRequired<Exclude<T[P], undefined | null>>;
+  [P in keyof T]-?: NestedRequired<Exclude<T[P], undefined | null>>;
 };
 
 export type RequiredOptions = NestedRequired<Options>;
 
 const defaultOptions: RequiredOptions = {
-	external: 'utools-api-types',
-	preload: {
-		path: './src/preload.ts',
-		watch: true,
-		name: 'window.preload',
-	},
-	buildUpx: {
-		pluginPath: './plugin.json',
-		outDir: 'upx',
-		outName: '[pluginName]_[version].upx',
-	},
+  external: "utools-api-types",
+  preload: {
+    path: "./src/preload.ts",
+    watch: true,
+    name: "window.preload",
+  },
+  buildUpx: {
+    pluginPath: "./plugin.json",
+    outDir: "upx",
+    outName: "[pluginName]_[version].upx",
+  },
 };
 
 export const resolveOptions = (options: Options): RequiredOptions =>
-	Object.entries(defaultOptions).reduce((ret, [key, v1]) => {
-		// @ts-ignore
-		const v2 = options[key];
+  Object.entries(defaultOptions).reduce((ret, [key, v1]) => {
+    // @ts-ignore
+    const v2 = options[key];
 
-		ret[key] = isUndef(v2) ? v1 : isObject(v1) && isObject(v2) ? { ...v1, ...v2 } : v2;
+    ret[key] = isUndef(v2)
+      ? v1
+      : isObject(v1) && isObject(v2)
+      ? { ...v1, ...v2 }
+      : v2;
 
-		return ret;
-	}, {} as any);
+    return ret;
+  }, {} as any);
